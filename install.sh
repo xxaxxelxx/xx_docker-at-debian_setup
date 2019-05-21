e11263d2275ab8fd34e86961ce6048355487935d
xxaxxelxx <x@axxel.net>
2019-05-21 09:17:11 +0200

    first move
    	neue Datei:     install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get update
+apt-cache policy docker-ce
+apt-get -y install docker-ce
+
+exit 0
